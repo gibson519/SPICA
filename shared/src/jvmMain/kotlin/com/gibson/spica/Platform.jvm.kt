a997f6a8d11abba4415d24c923bc0ff53321ff1d
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-27 19:08:46 +0100

    Update Platform.jvm.kt

--- shared/src/jvmMain/kotlin/com/gibson/spica/Platform.jvm.kt
+++ shared/src/jvmMain/kotlin/com/gibson/spica/Platform.jvm.kt
@@ -1,7 +1,8 @@
 package com.gibson.spica
 
-class JVMPlatform: Platform {
+class JVMPlatform : Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
+    override val type: PlatformType = PlatformType.Desktop
 }
 
 actual fun getPlatform(): Platform = JVMPlatform()
