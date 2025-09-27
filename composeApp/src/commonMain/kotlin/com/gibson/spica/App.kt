fb85bbe0b512968cfe24fa964ee818454ea11578
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-27 19:14:36 +0100

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/gibson/spica/App.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/App.kt
@@ -1,48 +1,31 @@
 package com.gibson.spica
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import spica.composeapp.generated.resources.Res
-import spica.composeapp.generated.resources.compose_multiplatform
+import com.gibson.spica.navigation.BottomNavigationBar
+import com.gibson.spica.navigation.NavigationItem
+import com.gibson.spica.navigation.SideNavigationBar
 
 @Composable
-@Preview
 fun App() {
+    val platform = getPlatform()
+    var selectedItem by remember { mutableStateOf<NavigationItem>(NavigationItem.Home) }
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        Surface {
+            when (platform.type) {
+                PlatformType.Android,
+                PlatformType.iOS -> BottomNavigationBar(
+                    selectedItem = selectedItem,
+                    onItemSelected = { selectedItem = it }
+                )
+
+                PlatformType.Desktop,
+                PlatformType.Web -> SideNavigationBar(
+                    selectedItem = selectedItem,
+                    onItemSelected = { selectedItem = it }
+                )
             }
         }
     }
