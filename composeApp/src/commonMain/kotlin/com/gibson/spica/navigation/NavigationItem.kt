06cdcfe27ef1fb1ebf6c9cf17506eedff357c01c
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-27 20:17:48 +0100

    Create NavigationItem.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/navigation/NavigationItem.kt
@@ -0,0 +1,22 @@
+package com.gibson.spica.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.material.icons.filled.Work
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class NavigationItem(
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home : NavigationItem("Home", Icons.Filled.Home)
+    object Marketplace : NavigationItem("Marketplace", Icons.Filled.Public)
+    object Portfolio : NavigationItem("Portfolio", Icons.Filled.Work)
+    object Profile : NavigationItem("Profile", Icons.Filled.Person)
+
+    companion object {
+        val items = listOf(Home, Marketplace, Portfolio, Profile)
+    }
+}
