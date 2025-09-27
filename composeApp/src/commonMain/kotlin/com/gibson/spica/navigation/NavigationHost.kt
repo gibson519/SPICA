22bc59f7280a9dc0d1592a56e23131a61f3b17e1
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-27 20:21:26 +0100

    Create NavigationHost.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/navigation/NavigationHost.kt
@@ -0,0 +1,17 @@
+package com.gibson.spica.navigation
+
+import androidx.compose.runtime.Composable
+import com.gibson.spica.ui.HomeScreen
+import com.gibson.spica.ui.MarketplaceScreen
+import com.gibson.spica.ui.PortfolioScreen
+import com.gibson.spica.ui.ProfileScreen
+
+@Composable
+fun NavigationHost(selectedItem: NavigationItem) {
+    when (selectedItem) {
+        is NavigationItem.Home -> HomeScreen()
+        is NavigationItem.Marketplace -> MarketplaceScreen()
+        is NavigationItem.Portfolio -> PortfolioScreen()
+        is NavigationItem.Profile -> ProfileScreen()
+    }
+}
