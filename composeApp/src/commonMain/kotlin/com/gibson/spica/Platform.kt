b85ee37ad5dc9daf965220ebab3ab575c83da9b3
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-29 13:06:13 +0100

    Update Platform.kt

--- composeApp/src/commonMain/kotlin/com/gibson/spica/Platform.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/Platform.kt
@@ -1,15 +1,12 @@
+
 package com.gibson.spica
 
-enum class PlatformType{ Android, iOS, Desktop, Web }
+enum class PlatformType { Android, iOS, Desktop, Web }
 
 interface Platform {
     val name: String
     val type: PlatformType
 }
 
-fun getPlatform(): Platform{
-    return Platform("Android", PlatformType.Android)
-    return Platform("iOS", PlatformType.iOS)
-    return Platform("Desktop", PlatformType.Desktop)
-    return Platform("Web", PlatformType.Web)
-}
+// 👇 only declare expect, no implementation here
+expect fun getPlatform(): Platform
