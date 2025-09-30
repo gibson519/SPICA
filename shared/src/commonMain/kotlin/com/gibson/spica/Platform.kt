9ea545b927e95df5b5cc5ae7225c903112bf1780
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-30 12:12:33 +0100

    Update Platform.kt

--- shared/src/commonMain/kotlin/com/gibson/spica/Platform.kt
+++ shared/src/commonMain/kotlin/com/gibson/spica/Platform.kt
@@ -1,10 +1,7 @@
 package com.gibson.spica
 
-enum class PlatformType { Android, iOS, Desktop, Web }
-
 interface Platform {
     val name: String
-    val type: PlatformType
 }
 
 expect fun getPlatform(): Platform
