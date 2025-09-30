177f85ffa01cc66cc4781566501a906381248da2
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-30 12:06:50 +0100

    Delete composeApp/src/jvmMain/kotlin/com/gibson/spica/Platform.Jvm.kt

--- composeApp/src/jvmMain/kotlin/com/gibson/spica/Platform.Jvm.kt
+++
@@ -1,8 +0,0 @@
-package com.gibson.spica
-
-class JVMPlatform : Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-    override val type: PlatformType = PlatformType.Desktop
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
