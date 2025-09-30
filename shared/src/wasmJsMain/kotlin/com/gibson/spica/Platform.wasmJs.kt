ceccbdab6dd3cfecb206f6a7e3071d7f21d4db92
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-30 12:17:49 +0100

    Update Platform.wasmJs.kt

--- shared/src/wasmJsMain/kotlin/com/gibson/spica/Platform.wasmJs.kt
+++ shared/src/wasmJsMain/kotlin/com/gibson/spica/Platform.wasmJs.kt
@@ -2,7 +2,6 @@ package com.gibson.spica
 
 class WasmPlatform : Platform {
     override val name: String = "Web with Kotlin/Wasm"
-    override val type: PlatformType = PlatformType.Web
 }
 
 actual fun getPlatform(): Platform = WasmPlatform()
