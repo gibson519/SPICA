c7e630478a726c6926d118f48d0304abb87e08c3
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-30 12:17:10 +0100

    Update Platform.js.kt

--- shared/src/jsMain/kotlin/com/gibson/spica/Platform.js.kt
+++ shared/src/jsMain/kotlin/com/gibson/spica/Platform.js.kt
@@ -2,7 +2,6 @@ package com.gibson.spica
 
 class JsPlatform : Platform {
     override val name: String = "Web with Kotlin/JS"
-    override val type: PlatformType = PlatformType.Web
 }
 
 actual fun getPlatform(): Platform = JsPlatform()
