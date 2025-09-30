c979da5a4d9d52e04656b4fb77eb33a82742e2b0
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-09-30 13:10:40 +0100

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/gibson/spica/App.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/App.kt
@@ -3,32 +3,19 @@ package com.gibson.spica
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
+import com.gibson.spica.navigation.NavigationBar
 import com.gibson.spica.navigation.NavigationItem
-import com.gibson.spica.navigation.NavigationLayout
-import com.gibson.spica.navigation.NavigationScaffold
 
 @Composable
 fun App() {
-    val platform = getPlatform()
     var selectedItem by remember { mutableStateOf<NavigationItem>(NavigationItem.Home) }
 
     MaterialTheme {
         Surface {
-            when (platform.type) {
-                PlatformType.Android,
-                PlatformType.iOS -> NavigationScaffold(
-                    layout = NavigationLayout.Bottom,
-                    selectedItem = selectedItem,
-                    onItemSelected = { selectedItem = it }
-                )
-
-                PlatformType.Desktop,
-                PlatformType.Web -> NavigationScaffold(
-                    layout = NavigationLayout.Side,
-                    selectedItem = selectedItem,
-                    onItemSelected = { selectedItem = it }
-                )
-            }
+            NavigationBar(
+                selectedItem = selectedItem,
+                onItemSelected = { selectedItem = it }
+            )
         }
     }
 }
