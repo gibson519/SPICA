0894dbc7a247f1bfe2e8b1cdad224389cb10bdd3
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-10-01 11:12:23 +0100

    Create HomeScreen.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/ui/screens/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.gibson.spica.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun HomeScreen() {
+    Text("Home Screen", color = Color.White)
+}
