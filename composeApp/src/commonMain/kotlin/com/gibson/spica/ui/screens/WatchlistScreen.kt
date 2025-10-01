fde9691b1de60529f9a849a8f4d5c0414dadd6fe
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-10-01 11:14:21 +0100

    Create WatchlistScreen.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/ui/screens/WatchlistScreen.kt
@@ -0,0 +1,10 @@
+package com.gibson.spica.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun WatchlistScreen() {
+    Text("Watchlist Screen", color = Color.White)
+}
