3bda5d7983c14a246e4c5a4cebafbc4ec9c0e60b
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-10-03 07:07:40 +0100

    Update PlatformNavBar.kt

--- composeApp/src/webMain/kotlin/com/gibson/spica/navigation/PlatformNavBar.kt
+++ composeApp/src/webMain/kotlin/com/gibson/spica/navigation/PlatformNavBar.kt
@@ -3,7 +3,7 @@ package com.gibson.spica.navigation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
