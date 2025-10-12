1eca2ea5115ecf28e88a72769c9350d0df26876c
Mrgithub <githubactions518@gmail.com>
2025-10-12 23:01:40 +0100

    Create DefaultSpicaTabs.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/spica/ui/DefaultSpicaTabs.kt
@@ -0,0 +1,14 @@
+package com.gibson.spica.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Work
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Public
+
+val DefaultSpicaTabs = listOf(
+    SpicaTab("Home", Icons.Default.Home),
+    SpicaTab("Portfolio", Icons.Default.Work),
+    SpicaTab("Watchlist", Icons.Default.List),
+    SpicaTab("Markets", Icons.Default.Public)
+)
