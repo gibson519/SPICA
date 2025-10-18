628e7d9b4f4a3496808c517b6ecc2d9ce820af62
Mrgithub <githubactions518@gmail.com>
2025-10-18 21:53:28 +0100

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation("androidx.navigation:navigation-compose:2.8.0")
         }
         commonMain.dependencies {
             implementation(compose.runtime)
