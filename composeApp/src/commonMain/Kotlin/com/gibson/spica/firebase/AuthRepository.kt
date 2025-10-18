edbc39f46638297357a6742085e5eea2ab1b443d
Mrgithub <githubactions518@gmail.com>
2025-10-18 20:05:16 +0100

    Create AuthRepository.kt

---
+++ composeApp/src/commonMain/Kotlin/com/gibson/spica/firebase/AuthRepository.kt
@@ -0,0 +1,20 @@
+package com.gibson.spica.auth
+
+import com.gibson.spica.firebase.FirebaseAuthService
+
+class AuthRepository(
+    private val firebaseAuth: FirebaseAuthService = FirebaseAuthService()
+) {
+    suspend fun signUp(email: String, password: String): AuthResult =
+        firebaseAuth.signUp(email, password)
+
+    suspend fun signIn(email: String, password: String): AuthResult =
+        firebaseAuth.signIn(email, password)
+
+    suspend fun sendEmailVerification() = firebaseAuth.sendEmailVerification()
+    suspend fun reloadUser() = firebaseAuth.reloadUser()
+    suspend fun signOut() = firebaseAuth.signOut()
+
+    fun currentUserId() = firebaseAuth.currentUserId()
+    fun isEmailVerified() = firebaseAuth.isEmailVerified()
+}
