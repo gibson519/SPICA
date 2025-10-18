835f47b3a982b451e4a10356172075dc64faff4c
Mrgithub <githubactions518@gmail.com>
2025-10-18 22:22:22 +0100

    Update AuthResult.kt

--- composeApp/src/commonMain/Kotlin/com/gibson/spica/firebase/AuthResult.kt
+++ composeApp/src/commonMain/Kotlin/com/gibson/spica/firebase/AuthResult.kt
@@ -1,6 +1,11 @@
 package com.gibson.spica.firebase
 
+/**
+ * Represents the result of an authentication action.
+ */
 data class AuthResult(
-    val userId: String?,
-    val emailVerified: Boolean,
+    val userId: String? = null,
+    val email: String? = null,
+    val isVerified: Boolean = false,
+    val error: String? = null
 )
