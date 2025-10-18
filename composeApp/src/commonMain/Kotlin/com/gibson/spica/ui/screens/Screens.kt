47bb2b9f3c96527f22ff9924acef3d5daf78a7c2
Mrgithub <githubactions518@gmail.com>
2025-10-18 21:16:25 +0100

    Create Screens.kt

---
+++ composeApp/src/commonMain/Kotlin/com/gibson/spica/ui/screens/Screens.kt
@@ -0,0 +1,9 @@
+package com.gibson.spica.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable fun HomeScreen() = Text("Home Screen")
+@Composable fun PortfolioScreen() = Text("Portfolio Screen")
+@Composable fun WatchlistScreen() = Text("Watchlist Screen")
+@Composable fun MarketScreen() = Text("Market Screen")
